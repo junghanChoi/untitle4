06d34f7a9cfc2e9c3aea94b36a4e0fde6ea8bc37
junghanChoi <david.junghan91@gmail.com>
2021-05-11 19:04:34 +0900

    b1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public static void branch1(){
+        System.out.println("ranch!");
+    }
 }
