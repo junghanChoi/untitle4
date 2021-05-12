84af421c3e7ec29775f593328023a796ccef3df2
junghanChoi <david.junghan91@gmail.com>
2021-05-12 19:54:19 +0900

    call branch at main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        branch1();
     }
     public static void branch1(){
         System.out.println("ranch!");
